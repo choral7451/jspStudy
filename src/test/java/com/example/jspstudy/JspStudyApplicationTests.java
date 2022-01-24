378ef3f432194d06ea005a91072153c25c2da059
Daniel Im <chorales@naver.com>
2022-01-24 23:18:54 +0900

    Initial Commit

---
+++ src/test/java/com/example/jspstudy/JspStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jspstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
